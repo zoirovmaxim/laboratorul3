3dae86385899c02d3cf8c5ace778ab82f08ac553
Furtuna Eugeniu <[200~>
2024-02-06 11:11:44 +0200

    Relatii, migrare route

---
+++ database/migrations/2024_02_06_083639_create_phones_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->usingnedBigInteger('user_id');
+            $table->forign('user_id')->reference('id')
+            ->on('user')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phones');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+
 Route::redirect('/', '/users');
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create',[UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
+//Phone
 
 
 
